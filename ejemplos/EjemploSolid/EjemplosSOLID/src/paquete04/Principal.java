36d418aa28d0458a22c4019cf1939fe2ce514dfa
Giafranco35 <sanchezgian58@gmail.com>
2021-07-14 16:52:03 -0500

    trabajo

--- ejemplos/EjemploSolid/EjemplosSOLID/src/paquete04/Principal.java
+++ ejemplos/EjemploSolid/EjemplosSOLID/src/paquete04/Principal.java
@@ -2,13 +2,16 @@
  * O — Open/closed principle (Principio abierto/cerrado)
  */
 package paquete04;
+import java.util.ArrayList;
 
 /**
  *
  * @author reroes
  */
+
 public class Principal {
     public static void main(String[] args) {
+        
         TransporteBus bus = new TransporteBus();
         bus.establecerCooperativaBus("24 Mayo");
         bus.establecerTarifa();
@@ -17,9 +20,28 @@ public class Principal {
         taxi.establecerCooperativaTaxi("Yahuarcuna");
         taxi.establecerTarifa();
         
+        TransporteTranvia tra = new TransporteTranvia();
+        tra.setTranvia("Tranvia 1");
+        tra.establecerTarifa();
+        
+        TransporteAereo ae = new TransporteAereo();
+        ae.setAereo("avion ");
+        ae.establecerTarifa();
+        
+        TransporteMaritimo mar = new TransporteMaritimo();
+        mar.setMaritimo("Barco 1");
+        mar.establecerTarifa();
+ 
         TiposTransporte tipos = new TiposTransporte();
-        tipos.establecerTransporteBus(bus);
-        tipos.establecerTransporteTaxi(taxi);
+        
+        ArrayList<Transporte> lista = new ArrayList<>();
+        lista.add(bus);
+        lista.add(taxi);
+        lista.add(tra);
+        lista.add(ae);
+        lista.add(mar);
+        
+        tipos.establecerTransportes(lista);
         tipos.establecerPromedioTarifas();
         
         System.out.printf("Promedio de Tarifas: %.2f", 
